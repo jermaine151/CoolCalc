6a25380493e00ea80d84ee8ccbff8b77cec0b355
Jermaine L <jermaine151@gmail.com>
2017-01-09 18:22:10 -0500

    Initial commit

---
+++ app/src/main/java/org/jermaine151/coolcalc/CalcActivity.java
@@ -0,0 +1,13 @@
+package org.jermaine151.coolcalc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalcActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc);
+    }
+}
